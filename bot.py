6f67f29be20a4d3a236f49933eb130075fb6bbb5
y4r <48801392+y4rsh@users.noreply.github.com>
2021-02-05 14:28:42 +0300

    Echo bot using aiogram

--- bot.py
+++ bot.py
@@ -1,13 +1,53 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
 import config
-import telebot
+from aiogram import Bot, Dispatcher, executor, types
 
-bot = telebot.TeleBot(config.token)
+API_TOKEN = config.token
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, "хаха офигенно")
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    with open('data/cats.jpg', 'rb') as photo:
+        '''
+        # Old fashioned way:
+        await bot.send_photo(
+            message.chat.id,
+            photo,
+            caption='Cats are here 😺',
+            reply_to_message_id=message.message_id,
+        )
+        '''
+
+        await message.reply_photo(photo, caption='Cats are here 😺')
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    executor.start_polling(dp, skip_updates=True)
